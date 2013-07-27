59711f00a53355a353e6e7147cf60b459e863ee6
Swaroop C H <swaroop@swaroopch.com>
2013-07-27 12:13:14 +0530

    Use Leiningen 2.x

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://isbn.net.in"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"] ; https://github.com/clojure/clojure
                  [compojure "1.1.5"]    ; https://github.com/weavejester/compojure
                  [ring/ring-jetty-adapter "1.2.0"] ; https://github.com/ring-clojure/ring
